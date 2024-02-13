8cbb7b4ed0fe867ed11c099e94161932849e3590
Mustapha Adams <adamsmustapha419@gmail.com>
2024-02-13 12:36:41 +0000

    Create Two City Scheduling.py

---
+++ camp2/Two City Scheduling.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+
+        diff = sorted(costs, key=lambda x: x[0] - x[1])
+        l, r = 0, len(diff) - 1
+        minCost = 0
+
+        for i in range(len(costs) // 2):
+            minCost += diff[i][0]
+        
+        for i in range(len(costs) // 2, len(costs)):
+            minCost += diff[i][1]
+
+        return minCost
