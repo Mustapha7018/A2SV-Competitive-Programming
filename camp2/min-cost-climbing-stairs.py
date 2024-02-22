602f578d542c305e72acc7dba87d7b2e06eb8572
Mustapha Adams <adamsmustapha419@gmail.com>
2024-02-22 15:54:06 +0000

    Update min-cost-climbing-stairs.py

--- camp2/min-cost-climbing-stairs.py
+++ camp2/min-cost-climbing-stairs.py
@@ -1,3 +1,23 @@
+# BOTTOM UP APPROACH
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+
+        N = len(cost)
+        cost.append(0)
+        dp = [float('inf')] * (N+1)
+
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+
+        for i in range(2, N+1):
+            dp[i] = cost[i] + min(dp[i - 1], dp[i - 2])
+        
+        return dp[-1]
+
+
+
+
+# TOP DOWN APPROACH
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
 
