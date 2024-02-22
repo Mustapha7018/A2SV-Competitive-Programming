f2d276e6f02a52b8e7773bf02840d95673c89492
Mustapha Adams <adamsmustapha419@gmail.com>
2024-02-22 16:08:11 +0000

    Update n-th-tribonacci-number.py

--- camp2/n-th-tribonacci-number.py
+++ camp2/n-th-tribonacci-number.py
@@ -1,3 +1,23 @@
+# BOTTOM UP APPROACH
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        dp = [0] * (n + 1)
+
+        if n == 0 or n == 1: return n
+
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 1
+
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+        
+        print(dp)
+        return dp[-1]
+
+
+
+# TOP DOWN APPROACH
 class Solution:
     def tribonacci(self, n: int) -> int:
 
