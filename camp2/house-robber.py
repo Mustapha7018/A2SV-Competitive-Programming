9a3ff95dee627bc19aad6d59bde60c5a78d5a2e3
Mustapha Adams <adamsmustapha419@gmail.com>
2024-02-22 14:50:15 +0000

    Update house-robber.py

--- camp2/house-robber.py
+++ camp2/house-robber.py
@@ -1,3 +1,20 @@
+# BOTTOM UP APPROACH
+class Solution:
+    def rob(self, house: List[int]) -> int:
+
+        N = len(house)
+
+        dp = [0] * N
+        dp[0] = house[0]
+
+        for i in range(1, N):
+            dp[i] += max(house[i] + dp[i - 2], dp[i - 1])
+
+        return dp[-1]
+
+
+
+# TOP DOWN APPROACH
 class Solution:
     def rob(self, house: List[int]) -> int:
         N = len(house)
