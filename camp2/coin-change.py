f3da46abf7c682eaf22b8b6c8c30568064dbd97d
Mustapha Adams <adamsmustapha419@gmail.com>
2024-02-23 09:25:14 +0000

    Update coin-change.py

--- camp2/coin-change.py
+++ camp2/coin-change.py
@@ -1,3 +1,22 @@
+# BOTTOM UP APPROACH
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        N = len(coins)
+        dp = [float('inf')] * (amount + 1)
+
+        dp[0] = 0
+
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if i - coin >= 0:
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
+
+        return dp[amount] if dp[-1] != float('inf') else -1
+
+
+
+# TOP DOWN APPROACH
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
 
