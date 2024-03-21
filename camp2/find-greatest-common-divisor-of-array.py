a9d17fb69d0f3a29b8fb00fe41a907268cf41c89
Mustapha Adams <adamsmustapha419@gmail.com>
2024-03-21 17:24:44 +0000

    Update find-greatest-common-divisor-of-array.py

--- camp2/find-greatest-common-divisor-of-array.py
+++ camp2/find-greatest-common-divisor-of-array.py
@@ -1,14 +1,28 @@
+# using gcd function
 class Solution:
     def findGCD(self, nums: List[int]) -> int:
+        minn = min(nums)
+        maxx = max(nums)
 
-        maxx, minn = max(nums), min(nums)
+        def gcd(a, b):
+            if b == 0: return a
 
-        try:
-            max_ = max(i for i in range(2, minn + 1) if maxx % i == 0 and minn % i == 0)
-            return max_
+            return gcd(b, a % b)
         
-        except:
-            return 1
+        return gcd(maxx, minn)
+
+
+# class Solution:
+#     def findGCD(self, nums: List[int]) -> int:
+
+#         maxx, minn = max(nums), min(nums)
+
+#         try:
+#             max_ = max(i for i in range(2, minn + 1) if maxx % i == 0 and minn % i == 0)
+#             return max_
+        
+#         except:
+#             return 1
 
         
 
