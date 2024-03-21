75813c381616fd96eeb8ebeb5c2199d5cebd8682
Mustapha Adams <adamsmustapha419@gmail.com>
2024-03-21 00:31:02 +0000

    Update permutation-sequence.py

--- camp2/permutation-sequence.py
+++ camp2/permutation-sequence.py
@@ -1,3 +1,28 @@
+# bactracking
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+
+        res, curr = [], []
+
+        def backtrack():
+            if len(curr) == n:
+                res.append(curr.copy())
+                return
+
+            if len(res) == k:
+                return
+
+            for j in range(1, n+1):
+                if j not in curr:
+                    curr.append(j)
+                    backtrack()
+                    curr.pop()
+        backtrack()
+        return ''.join([str(num) for num in res[-1]])
+
+
+
+# itertools.permutations
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
 
